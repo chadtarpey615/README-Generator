eda660f9a399e1649f565323ebd1f894d6631ee5
Chad Tarpey <chadtarpey@gmail.com>
2020-10-03 21:33:23 -0500

    Updated markdown links

--- methods/markdown.js
+++ methods/markdown.js
@@ -10,9 +10,9 @@ function markDownStructure(data) {
 
 
 |[${data.table[0] ? data.table[0]: ""}](#description-description)|
-|[${data.table[1] ? data.table[1]: ""}](#how to use-how to use)|
+|[${data.table[1] ? data.table[1]: ""}](#usage)|
 |[${data.table[2] ? data.table[2]: ""}](#License-License)|
-|[${data.table[3] ? data.table[3]: ""}](#author info-author-info)|
+|[${data.table[3] ? data.table[3]: ""}](#author-info-and-contact-information-for-questions)|
 
 ---------------------------------------     
     
